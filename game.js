43d7f0bfb50e675de1cd091327bf995164803f08
Markus Piotrowski <70880588+Mackapacka15@users.noreply.github.com>
2021-02-11 16:13:09 +0100

    Template For Everything Done

--- game.js
+++ game.js
@@ -2,3 +2,7 @@ function changeToRules() {
   document.querySelector(".rules").classList.toggle("hidden");
   document.querySelector(".menu").classList.toggle("hidden");
 }
+function changeToOpponentSelector() {
+  document.querySelector(".menu").classList.toggle("hidden");
+  document.querySelector(".Opponent-Selector").classList.toggle("hidden");
+}
