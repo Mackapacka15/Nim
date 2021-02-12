4e47b7f0b00725690f24366240f92591aada861e
Markus Piotrowski <70880588+Mackapacka15@users.noreply.github.com>
2021-02-12 09:15:50 +0100

    Game-Area-Started

--- game.js
+++ game.js
@@ -6,3 +6,13 @@ function changeToOpponentSelector() {
   document.querySelector(".menu").classList.toggle("hidden");
   document.querySelector(".Opponent-Selector").classList.toggle("hidden");
 }
+function changeToGame() {
+  document.querySelector(".Opponent-Selector").classList.toggle("hidden");
+  document.querySelector(".game-area").classList.toggle("hidden");
+}
+
+function opponentRandom() {
+  changeToGame();
+  let sticks = 0;
+  sticks = Math.random() * 60 + 1;
+}
